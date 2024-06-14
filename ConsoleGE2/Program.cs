b8dd55c8fd03dae416298e2f5d8400b183cce87f
DESKTOP-11KO03I\student <231075@st.yoshida-g.ac.jp>
2024-06-14 10:32:35 +0900

    疑似ネームハンターを作成

--- ConsoleGE2/Program.cs
+++ ConsoleGE2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace ConsoleGE2
             Player player = new Player();
             player.CreatePlayer();
             player.ShowPlayer();
+            player.PlayerBattle();
         }
     }
 
@@ -23,6 +25,7 @@ namespace ConsoleGE2
         private string[] atk = new string[2];
         public void CreatePlayer()
         {
+            //2人分のステータスを記入
             for (int i = 0; i < 2; i++)
             {
                 Console.Write((i + 1) + "人目の名前を入力:");
@@ -38,6 +41,7 @@ namespace ConsoleGE2
 
         public void ShowPlayer()
         {
+            //各ステータスを表示
             for (int i = 0; i < 2; i++)
             {
                 Console.WriteLine(name[i] + "のステータス");
@@ -45,6 +49,49 @@ namespace ConsoleGE2
                 Console.WriteLine("ATK:" + atk[i]);
             }
              Console.ReadLine();
+        }
+
+        public void PlayerBattle()
+        {
+            int[] HP = new int[2];
+            int[] ATK = new int[2];
+            int nowTurn = 0;
+            int waitTurn = 1;
+            //各ステータスをint化
+            for (int i = 0; i < 2; i++)
+            {
+                int.TryParse(hp[i], out HP[i]);
+                int.TryParse(atk[i], out ATK[i]);
+            }
+
+            while (true)
+            {
+
+                Console.WriteLine(name[nowTurn] + "の攻撃！" + name[waitTurn] + "に" + ATK[nowTurn] + "ダメージ");
+                HP[waitTurn] -= ATK[nowTurn];
+                Console.WriteLine(name[nowTurn] + "[HP:" + HP[nowTurn] + "]");
+                Console.WriteLine(name[waitTurn] + "[HP:" + HP[waitTurn] + "]");
+                Console.ReadLine();
+                if (HP[waitTurn] <= 0)
+                {
+                    Console.WriteLine(name[nowTurn] + "の勝ち"); 
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    if (nowTurn == 0)
+                    {
+                        nowTurn = 1;
+                        waitTurn = 0;
+                    }
+                    else
+                    {
+                        nowTurn = 0;
+                        waitTurn = 1;
+                    }
+                }
+            }
         }
     }
 }
